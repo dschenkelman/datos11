05c386450617c925d1b1de034aa07d07f77acbe8
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@f9f0a831-8b36-b70b-cf23-ca8cda4a889e>
2011-09-10 22:37:16 +0000

    Updated implementation of BlockFile

--- code/ElectronicElections/src/Block.cpp
+++ code/ElectronicElections/src/Block.cpp
@@ -7,16 +7,77 @@
 
 #include "Block.h"
 #include <string.h>
+#include <exception>
 
 Block::Block(int size) : maxSize(size)
 {
 	this->bytes = new char[this->maxSize];
 	memset(this->bytes, 0, this->maxSize);
+	this->freeSpace = this->maxSize;
 }
 
 char* Block::getBytes()
 {
 	return this->bytes;
+}
+
+int Block::getRecordCount()
+{
+	return this->recordCount;
+}
+
+void Block::updateInformation()
+{
+	int occupiedSize;
+	memcpy(&occupiedSize, this->bytes, 4);
+	this->freeSpace = this->maxSize - occupiedSize;
+
+    // first four bytes in each record represent record size
+
+	// ignore the first four bytes
+	int sum = 4;
+	int records = 0;
+	while(sum < occupiedSize)
+	{
+		int recordSize;
+		memcpy(&recordSize, this->bytes + sum, 4);
+		sum += recordSize;
+		records++;
+	}
+
+	this->recordCount = records;
+}
+
+int Block::getFreeSpace()
+{
+	return this->freeSpace;
+}
+
+void Block::forceInsert(Record *rec)
+{
+    int recSize = rec->getSize();
+    int occupiedSpace = this->maxSize - this->freeSpace;
+    // update block data
+    memcpy(this->bytes + occupiedSpace, rec->getBytes(), recSize);
+    // update block size
+    occupiedSpace += recSize;
+    memcpy(this->bytes, &occupiedSpace, 4);
+}
+
+void Block::insertRecord(Record *rec)
+{
+	if (!this->canInsertRecord(rec))
+	{
+		throw std::exception();
+	}
+
+	//TODO: Check that the record is unique
+    this->forceInsert(rec);
+}
+
+bool Block::canInsertRecord(Record *rec)
+{
+	return this->freeSpace >= rec->getSize();
 }
 
 Block::~Block()
--- code/ElectronicElections/src/Block.h
+++ code/ElectronicElections/src/Block.h
@@ -8,20 +8,33 @@
 #ifndef BLOCK_H_
 #define BLOCK_H_
 
+#include "Record.h"
+
 // represents a block. format:
 // B(contentSize(4 bytes), (registerSize(4 bytes), registerContent)*)
 class Block {
+	// -------------------FIELDS-------------------
+
 	// max size of the block [bytes]
 	int maxSize;
 	// occupied size of the block [bytes]
 	int freeSpace;
 	// content of the block
 	char* bytes;
+	// amount of records in the block
+	int recordCount;
 public:
 	Block(int size);
 	char* getBytes();
 	int getFreeSpace();
+	int getRecordCount();
+	// calculates the amount of records in the block
+	// and the amount of free space available in it
+	void updateInformation();
+	bool canInsertRecord(Record* rec);
+	void insertRecord(Record* rec);
 	virtual ~Block();
+    void forceInsert(Record *rec);
 };
 
 #endif /* BLOCK_H_ */

c72b95a06053e40be6cdb06397e8a4fe9d2223d7
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@f9f0a831-8b36-b70b-cf23-ca8cda4a889e>
2011-09-10 20:17:09 +0000

    Added initial eclipse project

---
+++ trunk/code/ElectronicVote/src/BlockFile.cpp
@@ -0,0 +1,17 @@
+/*
+ * BlockFile.cpp
+ *
+ *  Created on: Sep 10, 2011
+ *      Author: damian
+ */
+
+#include "BlockFile.h"
+
+BlockFile::BlockFile() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BlockFile::~BlockFile() {
+	// TODO Auto-generated destructor stub
+}

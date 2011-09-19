d0b349ebdf7a50b9d0a433c29111fd9001aecd44
damian.schenkelman@gmail.com <damian.schenkelman@gmail.com@f9f0a831-8b36-b70b-cf23-ca8cda4a889e>
2011-09-19 02:50:45 +0000

    Adding helper class for Byte operations

---
+++ code/ElectronicElections/src/Helpers/ByteOperators.h
@@ -0,0 +1,18 @@
+/*
+ * ByteOperators.h
+ *
+ *  Created on: Sep 18, 2011
+ *      Author: damian
+ */
+
+#ifndef BYTEOPERATORS_H_
+#define BYTEOPERATORS_H_
+
+class ByteOperators
+{
+public:
+	static bool isBitOne(char,int);
+	static void setBit(char*, char, char);
+};
+
+#endif /* BYTEOPERATORS_H_ */

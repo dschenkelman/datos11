151ccb6b325e1c19491ca0fe9d060607825a81ce
aletorrado@gmail.com <aletorrado@gmail.com@f9f0a831-8b36-b70b-cf23-ca8cda4a889e>
2011-09-28 00:41:49 +0000

    String hashing function which solves the limited-chars strings issue.

--- code/ElectronicElections/src/Hash/HashingFunctions.cpp
+++ code/ElectronicElections/src/Hash/HashingFunctions.cpp
@@ -49,3 +49,19 @@ int HashingFunctions::hashingElection(char* keyRecord)
 	// TODO: implement Election hashing depending its key value.
 	return 0;
 }
+
+long HashingFunctions::hashString(char* string) {
+	long res = 0;
+	long str_len = 0;
+	while (1) {
+		for (int i=0;i<16;i++) {
+			if (!*(string+i)) {
+				if (str_len > 16) return res;
+				return res*16/str_len;
+			}
+			if (*(string+i) < 48 || *(string+i) > 122) res+=*(string+i)*i*64;
+			res += (*(string+i)-48)*i*64;
+			str_len++;
+		}
+	}
+}

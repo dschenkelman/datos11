944bfbc2e911081589763fd397c3852e03fcacd6
ahogadosderazon@gmail.com <ahogadosderazon@gmail.com@f9f0a831-8b36-b70b-cf23-ca8cda4a889e>
2011-10-04 03:39:02 +0000

    electionslistmethods
    fichelo alguien porque tiré un poco de fruta en el compare

---
+++ code/ElectronicElections/src/Entities/ElectionsListMethods.h
@@ -0,0 +1,23 @@
+/*
+ * ElectionsListMethods.h
+ *
+ *  Created on: 04/10/2011
+ *      Author: gonzalo
+ */
+
+#ifndef ELECTIONSLISTMETHODS_H_
+#define ELECTIONSLISTMETHODS_H_
+
+#include "RecordMethods.h"
+
+class ElectionsListMethods: public RecordMethods {
+public:
+	ElectionsListMethods();
+	virtual int compare(const char* key, const char* recordBytes, int recordSize);
+	virtual void print(const char* recordBytes, int recordSize);
+	virtual void printKey(const char* key, int recordSize);
+	virtual VariableRecord* getKeyRecord(const char* recordBytes, int recordSize);
+	virtual ~ElectionsListMethods();
+};
+
+#endif /* ELECTIONSLISTMETHODS_H_ */

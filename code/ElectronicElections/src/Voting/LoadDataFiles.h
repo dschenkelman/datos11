4198698b15a056691f7239f9c023fa0242cba74c
gaby.ostro@gmail.com <gaby.ostro@gmail.com@f9f0a831-8b36-b70b-cf23-ca8cda4a889e>
2011-10-11 03:32:16 +0000

    Updated config file and logic to create all entities

--- code/ElectronicElections/src/Voting/LoadDataFiles.h
+++ code/ElectronicElections/src/Voting/LoadDataFiles.h
@@ -17,6 +17,8 @@
 #include "../Entities/VoterMethods.h"
 
 #include "../Entities/DistrictMethods.h"
+#include "../Entities/AdministratorMethods.h"
+#include "../Entities/Administrator.h"
 
 #include "../Entities/Charge.h"
 #include "../Hash/ChargeHashingFunction.h"
@@ -27,30 +29,66 @@ class LoadDataFiles
 {
 	std::string configName;
 	std::ifstream configFile;
-	HashBlockFile* hashVoterFile;
-	HashBlockFile* hashChargeFile;
-	Tree* treeDistrictFile;
-
-	int bSize;
-	int regsCount;
-	int regsSize;
-
+	// District attributes
+	std::string districtFileName;
+	int districtBlockSize;
+	// Election attributes
+	std::string electionFileName;
+	int electionBlockSize;
+	// ElectionList attributes
+	std::string electionListFileName;
+	int electionListBlockSize;
+	// Count attributes
+	std::string countFileName;
+	int countBlockSize;
+	// Candidate attributes
+	std::string candidateFileName;
+	int candidateBlockSize;
+	// Administrator attributes
+	std::string administratorFileName;
+	int administratorBlockSize;
 	// Voter attributes
+	std::string voterFileName;
 	int voterBlockSize;
 	int voterBlockAmount;
-
 	// Charge attributes
+	std::string chargeFileName;
 	int chargeBlockSize;
 	int chargeBlockAmount;
 
 	//void initializefile();
-	void readDistrictFile(char* dataFile);
-	void readVoterFile(char* dataFile);
-	void readChargeFile(char* dataFile);
+
+	void readDistrictFile(Tree* treeDistrictFile, char* dataFile);
+	void readVoterFile(HashBlockFile* hashVoterFile, char* dataFile);
+	void readChargeFile(HashBlockFile* hashChargeFile, char* dataFile);
 	void createFileType(char* fileType,char** fields);
 
 public:
 	LoadDataFiles(std::string configFileName);
+	bool canOpenAdminFile();
+	Tree createAdminFile();
+	Tree getAdminFile();
+
+	string getChargeFileName();
+	string getVoterFileName();
+	string getDistrictFileName();
+	string getElectionFileName();
+	string getElectionListFileName();
+	string getCountFileName();
+	string getCandidateFileName();
+	string getAdminFileName();
+
+	int getDistrictBlockSize();
+	int getElectionBlockSize();
+	int getElectionListBlockSize();
+	int getCountBlockSize();
+	int getCandidateBlockSize();
+	int getAdminBlockSize();
+
+	int getVoterBlockSize();
+	int getVoterBlockAmount();
+	int getChargeBlockSize();
+	int getChargeBlockAmount();
 
 	void readConfigFile();
 	~LoadDataFiles();

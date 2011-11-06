76d4994627e6fbf059dacbaa672a1758e01d6e0b
aletorrado@gmail.com <aletorrado@gmail.com@f9f0a831-8b36-b70b-cf23-ca8cda4a889e>
2011-11-06 14:59:48 +0000

    Generador de numeros primos

---
+++ trunk/code/ElectronicElections/src/RSA/PrimeGenerator.h
@@ -0,0 +1,26 @@
+/*
+ * PrimeGenerator.h
+ *
+ *  Created on: 06/11/2011
+ *      Author: alejandro
+ */
+
+#ifndef PRIMEGENERATOR_H_
+#define PRIMEGENERATOR_H_
+
+#include <iostream>
+using namespace std;
+
+class PrimeGenerator {
+private:
+	bool* primes;
+	int end;
+	int total;
+public:
+	PrimeGenerator(int end);
+	int totalGenerated();
+	void getAllPrimes(int* res);
+	virtual ~PrimeGenerator();
+};
+
+#endif /* PRIMEGENERATOR_H_ */
---
+++ trunk/code/ElectronicElections/src/RSA/PrimeGeneratorTests.h
@@ -0,0 +1,17 @@
+/*
+ * PrimeGeneratorTests.h
+ *
+ *  Created on: 06/11/2011
+ *      Author: alejandro
+ */
+
+#ifndef PRIMEGENERATORTESTS_H_
+#define PRIMEGENERATORTESTS_H_
+
+class PrimeGeneratorTests {
+public:
+	PrimeGeneratorTests();
+	virtual ~PrimeGeneratorTests();
+};
+
+#endif /* PRIMEGENERATORTESTS_H_ */

1c88915ca944f919e74cb5c328704a365f12f92b
ahogadosderazon@gmail.com <ahogadosderazon@gmail.com@f9f0a831-8b36-b70b-cf23-ca8cda4a889e>
2011-11-06 14:12:57 +0000

    RSA Cipher

---
+++ trunk/code/ElectronicElections/src/RSA/RSACipher.h
@@ -0,0 +1,17 @@
+/*
+ * RSACipher.h
+ *
+ *  Created on: 06/11/2011
+ *      Author: lejosdelcielo
+ */
+
+#ifndef RSACIPHER_H_
+#define RSACIPHER_H_
+
+class RSACipher {
+public:
+	RSACipher();
+	virtual ~RSACipher();
+};
+
+#endif /* RSACIPHER_H_ */

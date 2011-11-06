bb0f03793e37a5904c2ef0a8b62a4b21e1dcb93e
ahogadosderazon@gmail.com <ahogadosderazon@gmail.com@f9f0a831-8b36-b70b-cf23-ca8cda4a889e>
2011-11-06 14:10:17 +0000

    RSA

---
+++ trunk/code/ElectronicElections/src/RSA/RSAKey.h
@@ -0,0 +1,20 @@
+/*
+ * RSAKey.h
+ *
+ *  Created on: 06/11/2011
+ *      Author: gonzalo
+ */
+
+#ifndef RSAKEY_H_
+#define RSAKEY_H_
+
+typedef long long int64;
+
+struct RSAKey
+{
+	int64 n;
+	int64 exp;
+};
+
+
+#endif /* RSAKEY_H_ */

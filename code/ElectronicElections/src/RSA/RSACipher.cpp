842ae0222ccec1fc9a1056762056493e4d46a408
ahogadosderazon@gmail.com <ahogadosderazon@gmail.com@f9f0a831-8b36-b70b-cf23-ca8cda4a889e>
2011-11-06 14:12:57 +0000

    RSA Cipher

---
+++ code/ElectronicElections/src/RSA/RSACipher.cpp
@@ -0,0 +1,18 @@
+/*
+ * RSACipher.cpp
+ *
+ *  Created on: 06/11/2011
+ *      Author: lejosdelcielo
+ */
+
+#include "RSACipher.h"
+
+RSACipher::RSACipher() {
+	// TODO Auto-generated constructor stub
+
+}
+
+RSACipher::~RSACipher() {
+	// TODO Auto-generated destructor stub
+}
+

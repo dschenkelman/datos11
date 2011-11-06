a336cbe4ffc620b7a2a4c1ceceaa61fd3b3bb693
aletorrado@gmail.com <aletorrado@gmail.com@f9f0a831-8b36-b70b-cf23-ca8cda4a889e>
2011-11-06 19:59:10 +0000

    Modulo negativo

--- trunk/code/ElectronicElections/src/RSA/RSAKeySetTests.cpp
+++ trunk/code/ElectronicElections/src/RSA/RSAKeySetTests.cpp
@@ -25,15 +25,24 @@ void RSAKeySetTests::run()
 	this->printResult("Key Generation", testGeneration());
 }
 
+int64 RSAKeySetTests::negativeModulus(int64 base, int64 modulus) {
+	if (base>0) return base%modulus;
+	do {
+		base += modulus;
+	} while (base < 0);
+	return base;
+}
+
 bool RSAKeySetTests::testGeneration() {
-	RSAKeySet rsaks;
-	RSAKey privateKey = rsaks.getPrivateKey();
-	RSAKey publicKey = rsaks.getPublicKey();
-	cout << "phi " << rsaks.phi << endl;
-	cout << "privateexp " << privateKey.exp << endl;
-	cout << "publicexp " << publicKey.exp << endl;
-	cout << ((privateKey.exp%rsaks.phi)*(publicKey.exp%rsaks.phi))%rsaks.phi << endl;
-	return (( ((int64)privateKey.exp) * ((int64)publicKey.exp) )%rsaks.phi)==1;
+//	RSAKeySet rsaks;
+//	RSAKey privateKey = rsaks.getPrivateKey();
+//	RSAKey publicKey = rsaks.getPublicKey();
+//	cout << "phi " << rsaks.phi << endl;
+//	cout << "privateexp " << privateKey.exp << endl;
+//	cout << "publicexp " << publicKey.exp << endl;
+//	cout << negativeModulus(negativeModulus(privateKey.exp,rsaks.phi)*negativeModulus(publicKey.exp,rsaks.phi), rsaks.phi) << endl;
+//	return (( ((int64)privateKey.exp) * ((int64)publicKey.exp) )%rsaks.phi)==1;
+	return false;
 }
 
 RSAKeySetTests::~RSAKeySetTests()

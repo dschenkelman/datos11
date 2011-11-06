088e045fb914ca0508c49cb8236c224b4b782381
aletorrado@gmail.com <aletorrado@gmail.com@f9f0a831-8b36-b70b-cf23-ca8cda4a889e>
2011-11-06 14:59:48 +0000

    Generador de numeros primos

---
+++ code/ElectronicElections/src/RSA/PrimeGenerator.cpp
@@ -0,0 +1,48 @@
+/*
+ * PrimeGenerator.cpp
+ *
+ *  Created on: 06/11/2011
+ *      Author: alejandro
+ */
+
+#include "PrimeGenerator.h"
+
+PrimeGenerator::PrimeGenerator(int end) {
+	// two first values are not used for simplicity
+	this->end = end;
+	primes = new bool[end+1];
+	// initial setup: all true
+	for (int i=2;i<=end;i++) {
+		primes[i] = true;
+	}
+	this->total = end-1;
+	// starting algorithm
+	for (int i=2;i<=end/2;i++) {
+		if (primes[i] == true) {
+			for (int j=2*i;j<=end;j+=i) {
+				if (primes[j]) this->total--;
+				primes[j] = false;
+			}
+		}
+	}
+//	for (int i=0;i<end;i++) {
+//		primes[i]? cout << "true"<<endl : cout << "false"<<endl;
+//	}
+}
+
+int PrimeGenerator::totalGenerated() {
+	return this->total;
+}
+
+void PrimeGenerator::getAllPrimes(int* res) {
+	int position = 0;
+	for (int i=2;i<=end;i++) {
+		if (primes[i]) {
+			res[position++] = i;
+		}
+	}
+}
+
+PrimeGenerator::~PrimeGenerator() {
+	delete primes;
+}
---
+++ code/ElectronicElections/src/RSA/PrimeGeneratorTests.cpp
@@ -0,0 +1,31 @@
+/*
+ * PrimeGeneratorTests.cpp
+ *
+ *  Created on: 06/11/2011
+ *      Author: alejandro
+ */
+
+#include "PrimeGeneratorTests.h"
+
+PrimeGeneratorTests::PrimeGeneratorTests() {
+
+}
+
+PrimeGeneratorTests::~PrimeGeneratorTests() {
+
+}
+
+//#include "PrimeGenerator.h"
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	PrimeGenerator pg(7);
+//	int* res = new int[pg.totalGenerated()];
+//	pg.getAllPrimes(res);
+//	cout << "total"<<pg.totalGenerated()<<endl;
+//	for (int i=0;i<pg.totalGenerated();i++) {
+//		cout << res[i]<< endl;
+//	}
+//	return 0;
+//}

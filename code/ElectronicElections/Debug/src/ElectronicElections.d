9034896a7b2dc009d86adead717bc1f5b8957fd5
gaby.ostro@gmail.com <gaby.ostro@gmail.com@f9f0a831-8b36-b70b-cf23-ca8cda4a889e>
2011-11-13 19:43:19 +0000

    UML secuencia << RSA

--- code/ElectronicElections/Debug/src/ElectronicElections.d
+++ code/ElectronicElections/Debug/src/ElectronicElections.d
@@ -1,330 +1,143 @@
 src/ElectronicElections.d src/ElectronicElections.o: \
- ../src/ElectronicElections.cpp ../src/BlocksTests/RLVBlockFileTests.h \
- ../src/BlocksTests/./../VariableBlocks/SimpleVariableBlockFile.h \
- ../src/BlocksTests/./../VariableBlocks/SimpleVariableBlock.h \
- ../src/BlocksTests/./../VariableBlocks/VariableRecord.h \
- ../src/BlocksTests/./../VariableBlocks/RecordMethods.h \
- ../src/BlocksTests/./../VariableBlocks/BaseVariableBlock.h \
- ../src/BlocksTests/./../VariableBlocks/Constants.h \
- ../src/BlocksTests/./../VariableBlocks/BaseVariableBlockFile.h \
- ../src/BlocksTests/TreeBlockFileTests.h \
- ../src/BlocksTests/./../BPlusTree/TreeBlockFile.h \
- ../src/BlocksTests/./../BPlusTree/../VariableBlocks/BaseVariableBlockFile.h \
- ../src/BlocksTests/./../BPlusTree/TreeBlock.h \
- ../src/BlocksTests/./../BPlusTree/../VariableBlocks/BaseVariableBlock.h \
- ../src/BlocksTests/./../BPlusTree/FreeBlockManager.h \
- ../src/BPlusTreeTests/LeafNodeTests.h \
- ../src/BPlusTreeTests/../BPlusTree/LeafNode.h \
- ../src/BPlusTreeTests/../BPlusTree/Node.h \
- ../src/BPlusTreeTests/../BPlusTree/../VariableBlocks/RecordMethods.h \
- ../src/BPlusTreeTests/../BPlusTree/TreeBlock.h \
- ../src/BPlusTreeTests/../BPlusTree/OverflowParameter.h \
- ../src/BPlusTreeTests/../BPlusTree/SequenceTreeBlock.h \
- ../src/BPlusTreeTests/../BPlusTree/../VariableBlocks/VariableRecord.h \
- ../src/BPlusTreeTests/TreeTests.h \
- ../src/BPlusTreeTests/../BPlusTree/Tree.h \
- ../src/BPlusTreeTests/../BPlusTree/TreeBlockFile.h \
- ../src/Hash/HashTest.h ../src/Hash/HashBlockFile.h \
- ../src/Hash/../VariableBlocks/SimpleVariableBlockFile.h \
- ../src/Hash/HashingFunction.h ../src/Hash/HashBlock.h \
- ../src/Hash/../VariableBlocks/BaseVariableBlock.h \
- ../src/Hash/DistrictHashingFunction.h \
- ../src/Hash/../BlocksTests/CustomerMethods.h \
- ../src/Hash/../BlocksTests/../VariableBlocks/RecordMethods.h \
- ../src/Hash/../BlocksTests/Customer.h \
- ../src/Hash/../BlocksTests/../VariableBlocks/VariableRecord.h \
- ../src/EntitiesTests/DistrictTests.h \
- ../src/EntitiesTests/ElectionsListTests.h \
- ../src/EntitiesTests/CandidateTests.h \
- ../src/EntitiesTests/AdministratorTests.h \
- ../src/EntitiesTests/CountTests.h ../src/EntitiesTests/ChargeTests.h \
- ../src/Voting/Menu.h ../src/Voting/Voting.h \
- ../src/Voting/DataFileLoader.h ../src/Voting/../Hash/HashBlockFile.h \
- ../src/Voting/../BPlusTree/Tree.h ../src/Voting/../Entities/Voter.h \
- ../src/Voting/../Hash/VoterHashingFunction.h \
- ../src/Voting/../Hash/HashingFunction.h \
- ../src/Voting/../Entities/VoterMethods.h \
- ../src/Voting/../Entities/../VariableBlocks/RecordMethods.h \
- ../src/Voting/../Entities/DistrictMethods.h \
- ../src/Voting/../Entities/../VariableBlocks/VariableRecord.h \
- ../src/Voting/../Entities/../VariableBlocks/Constants.h \
- ../src/Voting/../Entities/District.h \
- ../src/Voting/../Entities/AdministratorMethods.h \
- ../src/Voting/../Entities/Administrator.h \
- ../src/Voting/../Entities/Charge.h \
- ../src/Voting/../Hash/ChargeHashingFunction.h \
- ../src/Voting/../Entities/ChargeMethods.h \
- ../src/Voting/../Entities/Candidate.h \
- ../src/Voting/../Entities/ElectionsList.h \
- ../src/Voting/../Entities/ElectionsListMethods.h \
- ../src/Voting/../Entities/CandidateMethods.h \
- ../src/Voting/../Entities/CountMethods.h \
- ../src/Voting/../Entities/Election.h \
- ../src/Voting/../Entities/ElectionMethods.h ../src/Voting/Log.h \
- ../src/Voting/Configuration.h ../src/Voting/ConfigurationEntry.h \
- ../src/Voting/../Entities/District.h \
- ../src/Voting/../Indexes/DistrictCountsIndex.h \
- ../src/Voting/../Indexes/../BPlusTree/Tree.h \
- ../src/Voting/../Indexes/../Entities/Count.h \
- ../src/Voting/../Indexes/DistrictCountsMethods.h \
- ../src/Voting/../Indexes/../VariableBlocks/RecordMethods.h \
- ../src/Hash/HashBlockFile.h ../src/Hash/DistrictHashingFunction.h \
- ../src/Hash/VoterHashingFunction.h ../src/Hash/ChargeHashingFunction.h \
- ../src/Entities/Administrator.h ../src/Entities/AdministratorMethods.h \
- ../src/Entities/Count.h ../src/Entities/District.h \
- ../src/Entities/DistrictMethods.h ../src/Entities/Voter.h \
- ../src/Entities/VoterMethods.h ../src/Entities/Election.h \
- ../src/Entities/ElectionMethods.h ../src/Entities/Charge.h \
- ../src/Entities/ChargeMethods.h ../src/Entities/ElectionsList.h \
- ../src/Entities/ElectionsListMethods.h ../src/Entities/Candidate.h \
- ../src/Entities/CandidateMethods.h \
- ../src/VariableBlocks/VariableRecord.h ../src/BPlusTree/Tree.h \
- ../src/Voting/Log.h ../src/Voting/Configuration.h \
- ../src/RSA/RSACipherTests.h ../src/RSA/RSACipher.h ../src/RSA/RSAKey.h \
- ../src/RSA/RSAKeySetTests.h ../src/RSA/RSAKeySet.h \
- ../src/Helpers/ValidationTests.h ../src/Helpers/Validation.h \
- ../src/Voting/DataFileLoader.h ../src/Indexes/DistrictElectionsIndex.h \
- ../src/Indexes/../BPlusTree/Tree.h ../src/Indexes/../Entities/Election.h \
- ../src/Indexes/DistrictElectionsMethods.h \
- ../src/Indexes/../VariableBlocks/RecordMethods.h \
- ../src/Indexes/DistrictCounts.h ../src/Indexes/CountId.h \
- ../src/Indexes/DistrictCountsMethods.h ../src/Indexes/CountId.h \
- ../src/Vigenere/VigenereCipher.h ../src/Vigenere/Kasisky.h
+ ../src/ElectronicElections.cpp ../src/Menu/MainMenu.h \
+ ../src/Menu/../Indexes/CountId.h ../src/Menu/../Voting/Configuration.h \
+ ../src/Menu/../Voting/ConfigurationEntry.h \
+ ../src/Menu/../Voting/DataFileLoader.h \
+ ../src/Menu/../Voting/../Hash/HashBlockFile.h \
+ ../src/Menu/../Voting/../Hash/../VariableBlocks/SimpleVariableBlockFile.h \
+ ../src/Menu/../Voting/../Hash/../VariableBlocks/SimpleVariableBlock.h \
+ ../src/Menu/../Voting/../Hash/../VariableBlocks/VariableRecord.h \
+ ../src/Menu/../Voting/../Hash/../VariableBlocks/RecordMethods.h \
+ ../src/Menu/../Voting/../Hash/../VariableBlocks/BaseVariableBlock.h \
+ ../src/Menu/../Voting/../Hash/../VariableBlocks/Constants.h \
+ ../src/Menu/../Voting/../Hash/../VariableBlocks/BaseVariableBlockFile.h \
+ ../src/Menu/../Voting/../Hash/HashingFunction.h \
+ ../src/Menu/../Voting/../Hash/HashBlock.h \
+ ../src/Menu/../Voting/../Hash/../VariableBlocks/BaseVariableBlock.h \
+ ../src/Menu/../Voting/../BPlusTree/Tree.h \
+ ../src/Menu/../Voting/../BPlusTree/../VariableBlocks/RecordMethods.h \
+ ../src/Menu/../Voting/../BPlusTree/TreeBlockFile.h \
+ ../src/Menu/../Voting/../BPlusTree/../VariableBlocks/BaseVariableBlockFile.h \
+ ../src/Menu/../Voting/../BPlusTree/TreeBlock.h \
+ ../src/Menu/../Voting/../BPlusTree/../VariableBlocks/BaseVariableBlock.h \
+ ../src/Menu/../Voting/../BPlusTree/FreeBlockManager.h \
+ ../src/Menu/../Voting/../BPlusTree/Node.h \
+ ../src/Menu/../Voting/../BPlusTree/OverflowParameter.h \
+ ../src/Menu/../Voting/../Entities/Voter.h \
+ ../src/Menu/../Voting/../Hash/VoterHashingFunction.h \
+ ../src/Menu/../Voting/../Entities/VoterMethods.h \
+ ../src/Menu/../Voting/../Entities/../VariableBlocks/RecordMethods.h \
+ ../src/Menu/../Voting/../Entities/DistrictMethods.h \
+ ../src/Menu/../Voting/../Entities/../VariableBlocks/VariableRecord.h \
+ ../src/Menu/../Voting/../Entities/../VariableBlocks/Constants.h \
+ ../src/Menu/../Voting/../Entities/District.h \
+ ../src/Menu/../Voting/../Entities/AdministratorMethods.h \
+ ../src/Menu/../Voting/../Entities/Administrator.h \
+ ../src/Menu/../Voting/../Entities/Charge.h \
+ ../src/Menu/../Voting/../Hash/ChargeHashingFunction.h \
+ ../src/Menu/../Voting/../Entities/ChargeMethods.h \
+ ../src/Menu/../Voting/../Entities/Candidate.h \
+ ../src/Menu/../Voting/../Entities/ElectionsList.h \
+ ../src/Menu/../Voting/../Entities/ElectionsListMethods.h \
+ ../src/Menu/../Voting/../Entities/CandidateMethods.h \
+ ../src/Menu/../Voting/../Entities/CountMethods.h \
+ ../src/Menu/../Voting/../Entities/Election.h \
+ ../src/Menu/../Voting/../Entities/ElectionMethods.h \
+ ../src/Menu/../Voting/Log.h ../src/Menu/../Voting/Configuration.h \
+ ../src/Voting/Menu.h
 
-../src/BlocksTests/RLVBlockFileTests.h:
+../src/Menu/MainMenu.h:
 
-../src/BlocksTests/./../VariableBlocks/SimpleVariableBlockFile.h:
+../src/Menu/../Indexes/CountId.h:
 
-../src/BlocksTests/./../VariableBlocks/SimpleVariableBlock.h:
+../src/Menu/../Voting/Configuration.h:
 
-../src/BlocksTests/./../VariableBlocks/VariableRecord.h:
+../src/Menu/../Voting/ConfigurationEntry.h:
 
-../src/BlocksTests/./../VariableBlocks/RecordMethods.h:
+../src/Menu/../Voting/DataFileLoader.h:
 
-../src/BlocksTests/./../VariableBlocks/BaseVariableBlock.h:
+../src/Menu/../Voting/../Hash/HashBlockFile.h:
 
-../src/BlocksTests/./../VariableBlocks/Constants.h:
+../src/Menu/../Voting/../Hash/../VariableBlocks/SimpleVariableBlockFile.h:
 
-../src/BlocksTests/./../VariableBlocks/BaseVariableBlockFile.h:
+../src/Menu/../Voting/../Hash/../VariableBlocks/SimpleVariableBlock.h:
 
-../src/BlocksTests/TreeBlockFileTests.h:
+../src/Menu/../Voting/../Hash/../VariableBlocks/VariableRecord.h:
 
-../src/BlocksTests/./../BPlusTree/TreeBlockFile.h:
+../src/Menu/../Voting/../Hash/../VariableBlocks/RecordMethods.h:
 
-../src/BlocksTests/./../BPlusTree/../VariableBlocks/BaseVariableBlockFile.h:
+../src/Menu/../Voting/../Hash/../VariableBlocks/BaseVariableBlock.h:
 
-../src/BlocksTests/./../BPlusTree/TreeBlock.h:
+../src/Menu/../Voting/../Hash/../VariableBlocks/Constants.h:
 
-../src/BlocksTests/./../BPlusTree/../VariableBlocks/BaseVariableBlock.h:
+../src/Menu/../Voting/../Hash/../VariableBlocks/BaseVariableBlockFile.h:
 
-../src/BlocksTests/./../BPlusTree/FreeBlockManager.h:
+../src/Menu/../Voting/../Hash/HashingFunction.h:
 
-../src/BPlusTreeTests/LeafNodeTests.h:
+../src/Menu/../Voting/../Hash/HashBlock.h:
 
-../src/BPlusTreeTests/../BPlusTree/LeafNode.h:
+../src/Menu/../Voting/../Hash/../VariableBlocks/BaseVariableBlock.h:
 
-../src/BPlusTreeTests/../BPlusTree/Node.h:
+../src/Menu/../Voting/../BPlusTree/Tree.h:
 
-../src/BPlusTreeTests/../BPlusTree/../VariableBlocks/RecordMethods.h:
+../src/Menu/../Voting/../BPlusTree/../VariableBlocks/RecordMethods.h:
 
-../src/BPlusTreeTests/../BPlusTree/TreeBlock.h:
+../src/Menu/../Voting/../BPlusTree/TreeBlockFile.h:
 
-../src/BPlusTreeTests/../BPlusTree/OverflowParameter.h:
+../src/Menu/../Voting/../BPlusTree/../VariableBlocks/BaseVariableBlockFile.h:
 
-../src/BPlusTreeTests/../BPlusTree/SequenceTreeBlock.h:
+../src/Menu/../Voting/../BPlusTree/TreeBlock.h:
 
-../src/BPlusTreeTests/../BPlusTree/../VariableBlocks/VariableRecord.h:
+../src/Menu/../Voting/../BPlusTree/../VariableBlocks/BaseVariableBlock.h:
 
-../src/BPlusTreeTests/TreeTests.h:
+../src/Menu/../Voting/../BPlusTree/FreeBlockManager.h:
 
-../src/BPlusTreeTests/../BPlusTree/Tree.h:
+../src/Menu/../Voting/../BPlusTree/Node.h:
 
-../src/BPlusTreeTests/../BPlusTree/TreeBlockFile.h:
+../src/Menu/../Voting/../BPlusTree/OverflowParameter.h:
 
-../src/Hash/HashTest.h:
+../src/Menu/../Voting/../Entities/Voter.h:
 
-../src/Hash/HashBlockFile.h:
+../src/Menu/../Voting/../Hash/VoterHashingFunction.h:
 
-../src/Hash/../VariableBlocks/SimpleVariableBlockFile.h:
+../src/Menu/../Voting/../Entities/VoterMethods.h:
 
-../src/Hash/HashingFunction.h:
+../src/Menu/../Voting/../Entities/../VariableBlocks/RecordMethods.h:
 
-../src/Hash/HashBlock.h:
+../src/Menu/../Voting/../Entities/DistrictMethods.h:
 
-../src/Hash/../VariableBlocks/BaseVariableBlock.h:
+../src/Menu/../Voting/../Entities/../VariableBlocks/VariableRecord.h:
 
-../src/Hash/DistrictHashingFunction.h:
+../src/Menu/../Voting/../Entities/../VariableBlocks/Constants.h:
 
-../src/Hash/../BlocksTests/CustomerMethods.h:
+../src/Menu/../Voting/../Entities/District.h:
 
-../src/Hash/../BlocksTests/../VariableBlocks/RecordMethods.h:
+../src/Menu/../Voting/../Entities/AdministratorMethods.h:
 
-../src/Hash/../BlocksTests/Customer.h:
+../src/Menu/../Voting/../Entities/Administrator.h:
 
-../src/Hash/../BlocksTests/../VariableBlocks/VariableRecord.h:
+../src/Menu/../Voting/../Entities/Charge.h:
 
-../src/EntitiesTests/DistrictTests.h:
+../src/Menu/../Voting/../Hash/ChargeHashingFunction.h:
 
-../src/EntitiesTests/ElectionsListTests.h:
+../src/Menu/../Voting/../Entities/ChargeMethods.h:
 
-../src/EntitiesTests/CandidateTests.h:
+../src/Menu/../Voting/../Entities/Candidate.h:
 
-../src/EntitiesTests/AdministratorTests.h:
+../src/Menu/../Voting/../Entities/ElectionsList.h:
 
-../src/EntitiesTests/CountTests.h:
+../src/Menu/../Voting/../Entities/ElectionsListMethods.h:
 
-../src/EntitiesTests/ChargeTests.h:
+../src/Menu/../Voting/../Entities/CandidateMethods.h:
+
+../src/Menu/../Voting/../Entities/CountMethods.h:
+
+../src/Menu/../Voting/../Entities/Election.h:
+
+../src/Menu/../Voting/../Entities/ElectionMethods.h:
+
+../src/Menu/../Voting/Log.h:
+
+../src/Menu/../Voting/Configuration.h:
 
 ../src/Voting/Menu.h:
-
-../src/Voting/Voting.h:
-
-../src/Voting/DataFileLoader.h:
-
-../src/Voting/../Hash/HashBlockFile.h:
-
-../src/Voting/../BPlusTree/Tree.h:
-
-../src/Voting/../Entities/Voter.h:
-
-../src/Voting/../Hash/VoterHashingFunction.h:
-
-../src/Voting/../Hash/HashingFunction.h:
-
-../src/Voting/../Entities/VoterMethods.h:
-
-../src/Voting/../Entities/../VariableBlocks/RecordMethods.h:
-
-../src/Voting/../Entities/DistrictMethods.h:
-
-../src/Voting/../Entities/../VariableBlocks/VariableRecord.h:
-
-../src/Voting/../Entities/../VariableBlocks/Constants.h:
-
-../src/Voting/../Entities/District.h:
-
-../src/Voting/../Entities/AdministratorMethods.h:
-
-../src/Voting/../Entities/Administrator.h:
-
-../src/Voting/../Entities/Charge.h:
-
-../src/Voting/../Hash/ChargeHashingFunction.h:
-
-../src/Voting/../Entities/ChargeMethods.h:
-
-../src/Voting/../Entities/Candidate.h:
-
-../src/Voting/../Entities/ElectionsList.h:
-
-../src/Voting/../Entities/ElectionsListMethods.h:
-
-../src/Voting/../Entities/CandidateMethods.h:
-
-../src/Voting/../Entities/CountMethods.h:
-
-../src/Voting/../Entities/Election.h:
-
-../src/Voting/../Entities/ElectionMethods.h:
-
-../src/Voting/Log.h:
-
-../src/Voting/Configuration.h:
-
-../src/Voting/ConfigurationEntry.h:
-
-../src/Voting/../Entities/District.h:
-
-../src/Voting/../Indexes/DistrictCountsIndex.h:
-
-../src/Voting/../Indexes/../BPlusTree/Tree.h:
-
-../src/Voting/../Indexes/../Entities/Count.h:
-
-../src/Voting/../Indexes/DistrictCountsMethods.h:
-
-../src/Voting/../Indexes/../VariableBlocks/RecordMethods.h:
-
-../src/Hash/HashBlockFile.h:
-
-../src/Hash/DistrictHashingFunction.h:
-
-../src/Hash/VoterHashingFunction.h:
-
-../src/Hash/ChargeHashingFunction.h:
-
-../src/Entities/Administrator.h:
-
-../src/Entities/AdministratorMethods.h:
-
-../src/Entities/Count.h:
-
-../src/Entities/District.h:
-
-../src/Entities/DistrictMethods.h:
-
-../src/Entities/Voter.h:
-
-../src/Entities/VoterMethods.h:
-
-../src/Entities/Election.h:
-
-../src/Entities/ElectionMethods.h:
-
-../src/Entities/Charge.h:
-
-../src/Entities/ChargeMethods.h:
-
-../src/Entities/ElectionsList.h:
-
-../src/Entities/ElectionsListMethods.h:
-
-../src/Entities/Candidate.h:
-
-../src/Entities/CandidateMethods.h:
-
-../src/VariableBlocks/VariableRecord.h:
-
-../src/BPlusTree/Tree.h:
-
-../src/Voting/Log.h:
-
-../src/Voting/Configuration.h:
-
-../src/RSA/RSACipherTests.h:
-
-../src/RSA/RSACipher.h:
-
-../src/RSA/RSAKey.h:
-
-../src/RSA/RSAKeySetTests.h:
-
-../src/RSA/RSAKeySet.h:
-
-../src/Helpers/ValidationTests.h:
-
-../src/Helpers/Validation.h:
-
-../src/Voting/DataFileLoader.h:
-
-../src/Indexes/DistrictElectionsIndex.h:
-
-../src/Indexes/../BPlusTree/Tree.h:
-
-../src/Indexes/../Entities/Election.h:
-
-../src/Indexes/DistrictElectionsMethods.h:
-
-../src/Indexes/../VariableBlocks/RecordMethods.h:
-
-../src/Indexes/DistrictCounts.h:
-
-../src/Indexes/CountId.h:
-
-../src/Indexes/DistrictCountsMethods.h:
-
-../src/Indexes/CountId.h:
-
-../src/Vigenere/VigenereCipher.h:
-
-../src/Vigenere/Kasisky.h:
